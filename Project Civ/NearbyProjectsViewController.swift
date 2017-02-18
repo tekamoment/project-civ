8dcc0ab09560f3c3b2016829d8a72a7048d4a24d
Carlos Arcenas <c.arcenas@me.com>
2017-02-18 15:23:06 +0800

    Implement Google map view.

---
+++ Project Civ/NearbyProjectsViewController.swift
@@ -0,0 +1,93 @@
+//
+//  NearbyProjectsViewController.swift
+//  Project Civ
+//
+//  Created by Carlos Arcenas on 2/18/17.
+//  Copyright © 2017 Rogue Three. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+
+import GoogleMaps
+
+class NearbyProjectsViewController: UIViewController {
+    
+    var locationManager: CLLocationManager!
+
+    var mapView: GMSMapView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+
+        // for the map
+        
+        extendedLayoutIncludesOpaqueBars = false
+        edgesForExtendedLayout = []
+        automaticallyAdjustsScrollViewInsets = false
+        
+        let camera = GMSCameraPosition.camera(withLatitude: 1.2925365, longitude: 103.7747835, zoom: 18.0)
+        let mapView = GMSMapView.map(withFrame: view.bounds, camera: camera)
+        mapView.isMyLocationEnabled = true
+        mapView.delegate = self
+        
+        self.mapView = mapView
+        //        print(mapView.myLocation)
+        
+        view.addSubview(mapView)
+        
+        
+        view.backgroundColor = UIColor.white
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        mapView?.frame = view.bounds
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension NearbyProjectsViewController: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // check if authorization passed
+        if status == .authorizedWhenInUse {
+            locationManager.startUpdatingLocation()
+            
+            mapView?.isMyLocationEnabled = true
+            mapView?.settings.myLocationButton = true
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        if let location = locations.first {
+            mapView?.camera = GMSCameraPosition(target: location.coordinate, zoom: 15, bearing: 0, viewingAngle: 0)
+            locationManager.stopUpdatingLocation()
+        }
+    }
+}
+
+extension NearbyProjectsViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapInfoWindowOf marker: GMSMarker) {
+        
+    }
+}
